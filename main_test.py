10fc726c73a4df00774a2b9b1cd43d61abd48502
Sang-hyun Park <hyun06000@gmail.com>
2021-09-12 18:43:27 +0900

    New test

--- main_test.py
+++ main_test.py
@@ -5,7 +5,7 @@ import main # import main.py
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         ret = main.helloworld("Test")
-        self.assertEqual(ret, "Hello World, Sang-hyun!")
+        self.assertEqual(ret, "Hello World, Test!")
 
 
 if __name__ == "__main__":
