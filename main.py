37207c792d2752e5bfc1d4f9adbf3aa28ed995fb
Sang-hyun Park <hyun06000@gmail.com>
2021-09-12 18:07:08 +0900

    main.py 20210912-black applied

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def      helloworld(a):
-    print(f"Hello world! {a}!")#hmm
-if __name__=="__main__"    :
+def helloworld(a):
+    print(f"Hello world! {a}!")  # hmm
+
+
+if __name__ == "__main__":
     helloworld("Sang-hyun")
